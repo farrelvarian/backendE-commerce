4650b55a94c38d193a5501f63ed3beccb2986c7c
Farrel Varian Eka Putra <42968960+farrelvarian@users.noreply.github.com>
2021-07-12 14:39:40 +0700

    initial commit

---
+++ src/routes/orders.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router()
+const orderController = require('../controllers/orders')
+
+router
+  .get('/', orderController.getAllOrder)
+  .get('/:id', orderController.getOrder)
+  .post('/', orderController.insertOrder)
+  .put('/:id', orderController.updateOrder)
+  .delete('/:id', orderController.deleteOrder)
+
+module.exports = router
---
+++ src/routes/users.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router()
+const userController = require('../controllers/users')
+
+router
+  .get('/', userController.getAllUser)
+  .get('/:id', userController.getUser)
+  .post('/', userController.insertUser)
+  .put('/:id', userController.updateUser)
+  .delete('/:id', userController.deleteUser)
+
+module.exports = router
