bb64f5008f30f7cae499ada5d6c04481520108cb
Farrel Varian Eka Putra <42968960+farrelvarian@users.noreply.github.com>
2021-07-12 11:15:39 +0700

    initial commit

--- src/models/users.js
+++ src/models/users.js
@@ -1,76 +1,76 @@
-const connection = require('../configs/db')
+const connection = require("../configs/db");
 
 const getAllUser = () => {
-  return new Promise((resolve, reject) => {
-    connection.query('SELECT * FROM users', (error, result) => {
-      if (!error) {
-        resolve(result)
-      } else {
-        reject(error)
-      }
-    })
-  })
-}
+    return new Promise((resolve, reject) => {
+        connection.query("SELECT * FROM users", (error, result) => {
+            if (!error) {
+                resolve(result);
+            } else {
+                reject(error);
+            }
+        });
+    });
+};
 
 const getUser = (id) => {
-  return new Promise((resolve, reject) => {
-    connection.query(
-      'SELECT * FROM users  WHERE id = ?',
-      id,
-      (error, result) => {
-        if (!error) {
-          resolve(result)
-        } else {
-          reject(error)
-        }
-      }
-    )
-  })
-}
+    return new Promise((resolve, reject) => {
+        connection.query(
+            "SELECT * FROM users  WHERE id = ?",
+            id,
+            (error, result) => {
+                if (!error) {
+                    resolve(result);
+                } else {
+                    reject(error);
+                }
+            }
+        );
+    });
+};
 
 const insertUser = (data) => {
-  return new Promise((resolve, reject) => {
-    connection.query('INSERT INTO users SET ?', data, (error, result) => {
-      if (!error) {
-        resolve(result)
-      } else {
-        reject(error)
-      }
-    })
-  })
-}
+    return new Promise((resolve, reject) => {
+        connection.query("INSERT INTO users SET ?", data, (error, result) => {
+            if (!error) {
+                resolve(result);
+            } else {
+                reject(error);
+            }
+        });
+    });
+};
 
 const updateUser = (id, data) => {
-  return new Promise((resolve, reject) => {
-    connection.query(
-      'UPDATE users SET ? WHERE id = ?', [data, id],
-      (error, result) => {
-        if (!error) {
-          resolve(result)
-        } else {
-          reject(error)
-        }
-      }
-    )
-  })
-}
+    return new Promise((resolve, reject) => {
+        connection.query(
+            "UPDATE users SET ? WHERE id = ?", [data, id],
+            (error, result) => {
+                if (!error) {
+                    resolve(result);
+                } else {
+                    reject(error);
+                }
+            }
+        );
+    });
+};
 
 const deleteUser = (id) => {
-  return new Promise((resolve, reject) => {
-    connection.query('DELETE FROM users WHERE id = ?', id, (error, result) => {
-      if (!error) {
-        resolve(result)
-      } else {
-        reject(error)
-      }
-    })
-  })
-}
+    return new Promise((resolve, reject) => {
+        connection.query("DELETE FROM users WHERE id = ?", id, (error, result) => {
+            if (!error) {
+                resolve(result);
+            } else {
+                reject(error);
+            }
+        });
+    });
+};
 
 module.exports = {
-  getAllUser,
-  getUser,
-  insertUser,
-  updateUser,
-  deleteUser
-}
+    getAllUser,
+    getUser,
+    insertUser,
+    updateUser,
+    deleteUser,
+};
